7f0f171152bab9e15ca4fe54388086e05ca3893d
AlbertoRodriguez4 <127990307+AlbertoRodriguez4@users.noreply.github.com>
2024-04-09 21:04:58 +0200

    a

--- Proyecto Disney/3-DisneySRC/src/navigation/Navigation.spec.js
+++
@@ -1,7 +0,0 @@
-describe('Navigation', () => {
-  it('renders all tabs correctly', () => {
-    const navigation = renderer.create(<Navigation />).toJSON();
-    expect(navigation).toMatchSnapshot();
-  });
-});
-
--- Proyecto Disney/3-DisneySRC/src/navigation/NavigationStacks.spec.js
+++
@@ -1,9 +0,0 @@
-describe('<NavigationStacks />', () => {
-  test('should render correctly', () => {
-    const component = renderer.create(
-      <NavigationStacks />
-    );
-    let tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
-  });
-});
